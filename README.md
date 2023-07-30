bd03953c67a184cdece7eff3054b843f5b637541
Mateus Santos <mateus.freira@gmail.com>
2023-07-30 10:26:49 -0300

    start

---
+++ README.md
@@ -0,0 +1,20 @@
+# OHIF Nun-db
+
+## Nun-db and OHIF Integration POC
+
+ This repository contains a proof-of-concept (POC) integration between Nun-db and OHIF (Open Health Imaging
+Foundation). The purpose of this experiment is to explore how Nun-db can facilitate multi-user interactions within
+OHIF, a widely-used open-source platform for medical imaging.
+
+## Overview
+
+ The integration aims to leverage Nun-db, a real-time browser-based database, to enhance the collaborative capabilities of OHIF. By utilizing Nun-db's real-time synchronization and data sharing features, we can enable multiple users to interact with OHIF simultaneously, providing a seamless and collaborative experience.
+
+## Features
+
+ - Real-time data synchronization: Nun-db allows real-time synchronization of data across multiple users, ensuring that any changes made by one user are immediately reflected for others.
+ - Multi-user interactions: With Nun-db, multiple users can simultaneously interact with OHIF, enabling collaborative viewing, annotation, and analysis of medical images.
+
+
+## Getting Started
+...
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,77 @@
-import { connect } from 'nun-db-react';
+import {
+    connect
+} from 'nun-db-react';
+import React from 'react';
+import * as cornerstone from '@cornerstonejs/core';
 
+const nunDb = connect(
+    React,
+    window.config.nunDb.url,
+    window.config.nunDb.db,
+    window.config.nunDb.user,
+    window.config.nunDb.token,
+    true
+);
 
 export default {
-	id: 'ohif-nun-db',
+    id: 'ohif-nun-db',
 
-  /**
-   * @param {object} params
-   * @param {object} params.configuration
-   * @param {ServicesManager} params.servicesManager
-   * @param {CommandsManager} params.commandsManager
-   * @returns void
-   */
-  async preRegistration({ servicesManager, commandsManager, configuration }) {
-    console.log('you will win sooner or latter!!');
-    // Registering new services
-    //servicesManager.registerService(MyNewService(servicesManager));
-    //const React = { useLayoutEffect: () =>{}};
-    // Todo fix connect int he nun-db react lib
-    //console.log(React);
-  },
+    /**
+     * @param {object} params
+     * @param {object} params.configuration
+     * @param {ServicesManager} params.servicesManager
+     * @param {CommandsManager} params.commandsManager
+     * @returns void
+     */
+    async preRegistration({
+        servicesManager,
+        commandsManager,
+        configuration
+    }) {
+        nunDb.watch('client-modeOpen', (event) => {
+            // Open the same exam
+            if (event.value === document.location.href) return;
+            document.location.href = event.value;
+        });
+        // Registering new services
+        //servicesManager.registerService(MyNewService(servicesManager));
+        //const React = { useLayoutEffect: () =>{}};
+        // Todo fix connect int he nun-db react lib
+        //console.log(React);
+    },
+    async onModeEnter() {
+        nunDb.set('client-modeOpen', document.location.href);
+        setTimeout(() => {
+            const viewport = cornerstone.getEnabledElements()[0].viewport;
+            const element = viewport.element;
+            console.log({
+                element
+            });
+
+            let ignore = false;
+            element.addEventListener(cornerstone.Enums.Events.CAMERA_MODIFIED, ((
+                evt: cornerstone.Types.EventTypes.CameraModifiedEvent
+            ) => {
+              if(!ignore) {
+                nunDb.set('client-camera', evt.detail.camera);
+                const currentImageIdIndex = viewport.getCurrentImageIdIndex();
+                nunDb.set('client-currentImageIdIndex', { currentImageIdIndex });
+              } else {
+                ignore = false;
+              }
+            }) as EventListener);
+            nunDb.watch('client-currentImageIdIndex', (event) => {
+                ignore = true;
+                viewport.setImageIdIndex(event.value.currentImageIdIndex);
+            })
+            nunDb.watch('client-camera', (event) => {
+                ignore = true;
+              viewport.setCamera(event.value);
+              viewport.render();
+            });
+        }, 500);
+    },
+    async onModeExit() {
+        nunDb.set('client-modeOpen', '/');
+    },
 };
