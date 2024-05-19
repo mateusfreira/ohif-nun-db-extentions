323be04c20107cf4cb7412f6f1484bc5eea4079e
Mateus Freira <mateus.freira@gmail.com>
2024-05-19 13:13:16 -0300

    Fix sync bug

--- src/index.ts
+++ src/index.ts
@@ -1,126 +1,147 @@
-import {
-    connect
-} from 'nun-db-react';
+import { connect } from 'nun-db-react';
 
 import React from 'react';
 import * as cornerstone from '@cornerstonejs/core';
 
+const config = window.config;
 
 const nunDbStateHolder = {
-    isRemoteControlEnabled: true,
+  isRemoteControlEnabled: true,
+  openTime: new Date().getTime(),
 };
-const nunDb = connect(
-    React,
-    window.config.nunDb.url,
-    window.config.nunDb.db,
-    window.config.nunDb.user,
-    window.config.nunDb.token,
-    true
-);
 
-const getCommandsModule = ({
-    servicesManager,
-}) => {
-    return {
-        definitions: {
-            enalbeNunDb: {
-                commandFn: () => {
-                    servicesManager.services.uiNotificationService.show({
-                        title: 'NunDb',
-                        message: 'Remote control enabled',
-                    });
-                    nunDbStateHolder.isRemoteControlEnabled = true;
-                },
-            },
-            disableNunDb: {
-                commandFn: () => {
-                    servicesManager.services.uiNotificationService.show({
-                        type: 'warning',
-                        title: 'NunDb',
-                        message: 'Remote control disabled',
-                    });
-                    nunDbStateHolder.isRemoteControlEnabled = false;
-                },
-            },
-        },
-        defaultContext: 'CORNERSTONE',
-    };
+function isFromThisClient(event: { value : { openTime: number } }) {
+  return nunDbStateHolder.openTime === event.value.openTime;
 }
 
-export default {
-    id: 'ohif-nun-db',
-    getCommandsModule,
-    /**
-     * @param {object} params
-     * @param {object} params.configuration
-     * @param {ServicesManager} params.servicesManager
-     * @param {CommandsManager} params.commandsManager
-     * @returns void
-     */
-    async preRegistration() {
-        nunDb.watch(`${window.config.nunDb.key}-modeOpen`, (event) => {
-            if(nunDbStateHolder.isRemoteControlEnabled) {
-                console.log('client-modeOpen', event);
-                const newAddress = new URL(event.value);
-                //Only change the address if the path or search has changed
-                if (newAddress.pathname !== document.location.pathname || newAddress.search !== document.location.search) {
-                    document.location.href = `${newAddress.pathname}${newAddress.search}`
-                }
-            }
-        });
+const nunDb = connect(
+  React,
+  config.nunDb.url,
+  config.nunDb.db,
+  config.nunDb.user,
+  config.nunDb.token,
+  true
+);
+
+const getCommandsModule = ({ servicesManager: any }) => {
+  return {
+    definitions: {
+      enalbeNunDb: {
+        commandFn: () => {
+          servicesManager.services.uiNotificationService.show({
+            title: 'NunDb',
+            message: 'Remote control enabled',
+          });
+          nunDbStateHolder.isRemoteControlEnabled = true;
+        },
+      },
+      disableNunDb: {
+        commandFn: () => {
+          servicesManager.services.uiNotificationService.show({
+            type: 'warning',
+            title: 'NunDb',
+            message: 'Remote control disabled',
+          });
+          nunDbStateHolder.isRemoteControlEnabled = false;
+        },
+      },
     },
-    async onModeEnter() {
-        nunDb.set(`${window.config.nunDb.key}-modeOpen`, document.location.href);
-        scheduleCornestoneCameraWatch();
-;
-    },
-    async onModeExit() {
-        nunDb.set(`${window.config.nunDb.key}-modeOpen`, document.location.href);
-    },
+    defaultContext: 'CORNERSTONE',
+  };
 };
 
-function scheduleCornestoneCameraWatch() {
-    setTimeout(() => {
-        const cornestoneElement = cornerstone.getEnabledElements()[0];
-        const isReady = !!cornestoneElement;
-        if (isReady) {
-
-            const viewport = cornerstone.getEnabledElements()[0].viewport;
-            const element = viewport.element;
-            console.log({
-                element
-            });
-
-            let ignore = false;
-            element.addEventListener(cornerstone.Enums.Events.CAMERA_MODIFIED, ((
-                evt: cornerstone.Types.EventTypes.CameraModifiedEvent
-            ) => {
-                if (!ignore) {
-                    nunDb.set(`${window.config.nunDb.key}-camera`, evt.detail.camera);
-                    const currentImageIdIndex = viewport.getCurrentImageIdIndex();
-                    nunDb.set(`${window.config.nunDb.key}-currentImageIdIndex`, {
-                        currentImageIdIndex
-                    });
-                } else {
-                    ignore = false;
-                }
-            }) as EventListener);
-            nunDb.watch(`${window.config.nunDb.key}-currentImageIdIndex`, (event) => {
-                if(nunDbStateHolder.isRemoteControlEnabled) {
-                    ignore = true;
-                    viewport.setImageIdIndex(event.value.currentImageIdIndex);
-                }
-            })
-            nunDb.watch(`${window.config.nunDb.key}-camera`, (event) => {
-                if(nunDbStateHolder.isRemoteControlEnabled) {
-                    ignore = true;
-                    viewport.setCamera(event.value);
-                    viewport.render();
-                }
-            });
-        } else {
-            console.log('not ready yet, will retry again in 500ms');
-            scheduleCornestoneCameraWatch();
+const NunDbExtentionConfig = {
+  id: 'ohif-nun-db',
+  getCommandsModule,
+  /**
+   * @param {object} params
+   * @param {object} params.configuration
+   * @param {ServicesManager} params.servicesManager
+   * @param {CommandsManager} params.commandsManager
+   * @returns void
+   */
+  async preRegistration() {
+    nunDb.watch(`${window.config.nunDb.key}-modeOpen`, (event: { value: string | URL }) => {
+      if (nunDbStateHolder.isRemoteControlEnabled) {
+        console.log('client-modeOpen', event);
+        const newAddress = new URL(event.value);
+        //Only change the address if the path or search has changed
+        if (
+          newAddress.pathname !== document.location.pathname ||
+          newAddress.search !== document.location.search
+        ) {
+          document.location.href = `${newAddress.pathname}${newAddress.search}`;
         }
-    }, 500);
+      }
+    });
+  },
+  async onModeEnter() {
+    nunDb.set(`${config.nunDb.key}-modeOpen`, document.location.href);
+    scheduleCornestoneCameraWatch();
+  },
+  async onModeExit() {
+    nunDb.set(`${config.nunDb.key}-modeOpen`, document.location.href);
+  },
+};
+
+export default NunDbExtentionConfig;
+
+function cornestoneEventListener(viewport: any, event: string, key: string) {
+  const element: Element = viewport.element;
+  const state = {
+    ignore: false,
+  };
+
+  element.addEventListener(event, evt => {
+    if (!state.ignore) {
+      state.ignore = true;
+      const eventPropagate = {
+        ...evt.detail.camera,
+        openTime: nunDbStateHolder.openTime,
+      };
+      nunDb.set(`${key}`, eventPropagate);
+      const currentImageIdIndex = viewport.getCurrentImageIdIndex();
+      nunDb.set(`${config.nunDb.key}-currentImageIdIndex`, {
+        openTime: nunDbStateHolder.openTime,
+        currentImageIdIndex,
+      });
+    } else {
+      state.ignore = false;
+    }
+  });
+
+  nunDb.watch(`${key}`, (event: { value: any; openTime: number }) => {
+    if (nunDbStateHolder.isRemoteControlEnabled && !isFromThisClient(event)) {
+      state.ignore = true;
+      viewport.setCamera(event['value']);
+      viewport.render();
+    }
+  });
+}
+
+function scheduleCornestoneCameraWatch() {
+  setTimeout(() => {
+    const cornestoneElement = cornerstone.getEnabledElements()[0];
+    const isReady = !!cornestoneElement;
+    if (isReady) {
+      const viewport = cornerstone.getEnabledElements()[0].viewport;
+      const element = viewport.element;
+      // Todo test event listener on OHIF
+      cornestoneEventListener(
+        viewport,
+        cornerstone.Enums.Events.CAMERA_MODIFIED,
+        `${config.nunDb.key}-camera`
+      );
+
+      nunDb.watch(`${config.nunDb.key}-currentImageIdIndex`, event => {
+        console.log('Here will watch current index');
+        if (nunDbStateHolder.isRemoteControlEnabled && !isFromThisClient(event)) {
+          viewport.setImageIdIndex(event.value.currentImageIdIndex);
+        }
+      });
+    } else {
+      console.log('not ready yet, will retry again in 500ms');
+      scheduleCornestoneCameraWatch();
+    }
+  }, 500);
 }
